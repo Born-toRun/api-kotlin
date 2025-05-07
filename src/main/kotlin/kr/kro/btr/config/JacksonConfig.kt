38a419eff2ba71af8b69678ac69fe9f504ed3220
small-goliath <meang617@gmail.com>
2025-05-07 20:30:10 +0900

    refactor(JacksonConfig.kt): JSON 직렬화 시 null 값 제외 설정 추가
    chore(application.yaml): 불필요한 Jackson 설정 제거

--- src/main/kotlin/kr/kro/btr/config/JacksonConfig.kt
+++ src/main/kotlin/kr/kro/btr/config/JacksonConfig.kt
@@ -1,5 +1,6 @@
 package kr.kro.btr.config
 
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
@@ -17,5 +18,6 @@ class JacksonConfig {
             .registerKotlinModule()
             .registerModule(JavaTimeModule())
             .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+            .setSerializationInclusion(JsonInclude.Include.NON_NULL)
     }
 }
